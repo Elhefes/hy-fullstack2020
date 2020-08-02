368446e0b369a9dcb308b4543991eefc3266574f
henripalin <henri.j.palin@gmail.com>
2020-08-02 14:14:05 +0300

    4.5 done

--- week4/blogilista/utils/list_helper.js
+++ week4/blogilista/utils/list_helper.js
@@ -8,7 +8,24 @@ const totalLikes = (blogs) => {
     return sum
 }
 
+const favouriteBlog = (blogs) => {
+    var index = 0
+    var mostLikes = 0
+
+    for (var i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > mostLikes) {
+            mostLikes = blogs[i].likes
+            index = i
+        }
+    }
+    const favouriteBlog = blogs[index]
+    JSON.stringify(favouriteBlog)
+
+    return favouriteBlog
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog
 }
