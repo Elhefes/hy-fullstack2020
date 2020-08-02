95b19547e73b08947a518a7fd4fa9b321913b39d
henripalin <henri.j.palin@gmail.com>
2020-08-02 21:09:19 +0300

    4.18 done

--- week4/blogilista/app.js
+++ week4/blogilista/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -19,5 +20,6 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
