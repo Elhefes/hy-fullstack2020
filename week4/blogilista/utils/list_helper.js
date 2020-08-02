8f2080a277842c828bc3e473b22498dcd25b227f
henripalin <henri.j.palin@gmail.com>
2020-08-02 13:34:42 +0300

    4.4 done

--- week4/blogilista/utils/list_helper.js
+++ week4/blogilista/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+    const sum = likes.reduce((a, b) => a + b, 0)
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
