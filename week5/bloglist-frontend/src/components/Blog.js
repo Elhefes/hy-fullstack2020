42f2ef510b99148d91de3e12a27764dc0884278e
henripalin <henri.j.palin@gmail.com>
2020-08-04 22:28:54 +0300

    5.6, 5.7

--- week5/bloglist-frontend/src/components/Blog.js
+++ week5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,47 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const infoHidden = { display: visible ? 'none' : '' }
+  const infoVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div style ={blogStyle}>
+      <div style={infoHidden}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={infoVisible}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={toggleVisibility}>hide</button>
+        </div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
