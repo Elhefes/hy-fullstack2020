bd49daac4876b64f41c327a6e11b480f093db7e3
henripalin <henri.j.palin@gmail.com>
2020-08-05 21:12:15 +0300

    5.9 done

--- week5/bloglist-frontend/src/App.js
+++ week5/bloglist-frontend/src/App.js
@@ -150,7 +150,7 @@ const App = () => {
         />
       </Togglable>
 
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} likeBlog={() => likeBlog(blog)} />
       )}
     </div>
