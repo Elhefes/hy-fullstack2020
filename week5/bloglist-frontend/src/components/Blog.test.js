653312c7bc644b4d963e502a98904ea12b1a808e
henripalin <henri.j.palin@gmail.com>
2020-08-06 20:43:10 +0300

    5.14-5.15

--- week5/bloglist-frontend/src/components/Blog.test.js
+++ week5/bloglist-frontend/src/components/Blog.test.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
-test('renders content', () => {
-    const blog = {
-      title: 'Test',
-      author: 'StannisB',
-      url: 'hwww.google.com/',
-      likes: 100,
-      user: {
-        username: 'Stannis',
-        name: 'StanTheMan',
-        id: '2346iu2i34go67g14763',
-      },
-    }
+const blog = {
+    title: 'Test',
+    author: 'StannisB',
+    url: 'hwww.google.com/',
+    likes: 100,
+    user: {
+      username: 'Stannis',
+      name: 'StanTheMan',
+      id: '2346iu2i34go67g14763',
+    },
+  }
 
-    const user = {
-        name: 'Seppo',
-        id: '1'
-      }
-      
-    const mockHandler = jest.fn()
-  
+const user = {
+  name: 'Seppo',
+  id: '1'
+}
+
+const mockHandler = jest.fn()
+
+test('renders content', () => {  
     const component = render(
       <Blog blog={blog} likeBlog = {mockHandler} removeBlog = {mockHandler} user={user} />
     )
@@ -30,3 +31,18 @@ test('renders content', () => {
     expect(component.container).toHaveTextContent('Test')
     expect(component.container).toHaveTextContent('StannisB')
   }) 
+
+test('clicking the button twice calls event handler once', async () => {
+  const likeMock = jest.fn()
+  const removeMock = jest.fn()
+
+  const { getByText } = render(
+    <Blog blog={blog} likeBlog = {likeMock} removeBlog = {removeMock} user={user} />
+  )
+  
+  const likeButton = getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(likeMock.mock.calls.length).toBe(2)
+})
