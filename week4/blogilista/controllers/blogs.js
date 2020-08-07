a7b69f0fffa23904c20cd6b4d1cbfb5e1698274b
henripalin <henri.j.palin@gmail.com>
2020-08-07 18:03:14 +0300

    5.17 done

--- week4/blogilista/controllers/blogs.js
+++ week4/blogilista/controllers/blogs.js
@@ -1,85 +1,67 @@
-const blogRouter = require('express').Router()
+const router = require('express').Router()
 const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
-blogRouter.get('/', async (request, response) => {
+router.get('/', async (request, response) => {
   const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 })
 
-  response.json(blogs.map(blog => blog.toJSON()))
-});
-
-blogRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
-  if (blog) {
-    response.json(blog.toJSON())
-  } else {
-    response.status(404).end()
-  }
+  response.json(blogs)
 })
 
-blogRouter.post('/', async (request, response) => {
-  const body = request.body
-  const token = getTokenFrom(request)
-  //const decodedToken = jwt.verify(token, process.env.SECRET)
+router.delete('/:id', async (request, response) => {
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
+
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() !== user.id.toString()) {
+    return response.status(401).json({ error: 'only the creator can delete blogs' })
+  }
+
+  await blog.remove()
+  user.blogs = user.blogs.filter(b => b.id.toString() !== request.params.id.toString())
+  await user.save()
+  response.status(204).end()
+})
+
+router.put('/:id', async (request, response) => {
+  const blog = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog.toJSON())
+})
+
+router.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
   const user = await User.findById(decodedToken.id)
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id
-  })
+  if (!blog.url || !blog.title) {
+    return response.status(400).send({ error: 'title or url missing ' })
+  }
 
-  if (blog.likes === undefined) {
+  if (!blog.likes) {
     blog.likes = 0
   }
 
-  if (blog.title == null || blog.url == null) {
-    response.status(400).end()
-  }
+  blog.user = user
+  const savedBlog = await blog.save()
 
-  const result = await blog.save()
-  user.blogs = user.blogs.concat(result._id)
+  user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
-  response.json(result.toJSON())
+
+  response.status(201).json(savedBlog)
 })
 
-blogRouter.delete('/:id', async (request, response, next) => {
-  const token = getTokenFrom(request)
-  //const decodedToken = jwt.verify(token, process.env.SECRET)
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  const blog = await Blog.findById(request.params.id)
-  if (blog.user.toString() === decodedToken.id.toString()) {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  }
-})
-
-blogRouter.put('/:id', async (request, response, next) => {
-  const blog = new Blog(request.body)
-  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-  response.status(200).json(result.toJSON())
-})
-
-
-module.exports = blogRouter;
+module.exports = router
