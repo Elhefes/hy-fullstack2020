fe2e45508d974751feb10ada5d0978a9a6b066dc
henripalin <henri.j.palin@gmail.com>
2020-11-10 19:09:49 +0200

    8.2

--- week8/library-backend/index.js
+++ week8/library-backend/index.js
@@ -87,13 +87,22 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+  }
+  type Book {
+    title: String!
+    published: Int!
+    author: String
+    genres: [String]
+    id: ID!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
