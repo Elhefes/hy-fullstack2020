b16bac60166f85a69616a9883b08f8554bb4c258
henripalin <henri.j.palin@gmail.com>
2021-06-07 23:22:43 +0300

    9.20

--- week9/patientor/frontend/src/PatientList/index.tsx
+++ week9/patientor/frontend/src/PatientList/index.tsx
@@ -2,40 +2,51 @@ import { useParams } from 'react-router-dom';
 import { apiBaseUrl } from "../constants";
 import React, { useEffect } from 'react';
 import axios from 'axios';
-import { Patient } from "../types";
 import { useStateValue, setPatient } from "../state";
 
 const PatientList: React.FC = () => {
-  const { id } = useParams< {id: string} >();
+  const { id } = useParams<{id: string}>();
   const [{ patients }, dispatch] = useStateValue();
+  const url = `${apiBaseUrl}/patients/${id}`
 
   useEffect(() => {
     const getPatient = async () => {
-      const { 
-        data: fetchedPatient
-        } = await axios.get<Patient>(
-        `${apiBaseUrl}/patients/${id}`
-      );
+      const { data: fetchedPatient } = await axios.get(url);
       console.log(fetchedPatient)
       dispatch(setPatient(fetchedPatient));
     }
 
-    if (patients[id] === undefined) return;
-
-    if (patients[id].entries === undefined) {
+    if (patients[id]?.entries === undefined) {
       getPatient();
     }
   }, []);
 
-  if (patients[id] === undefined) return null;
-
-  return (
-    <div>
-      <h2>{patients[id].name}</h2>
-      <h3>ssn: {patients[id].ssn}</h3>
-      <h3>occupation: {patients[id].occupation}</h3>
-    </div>
-  )
+  if (patients[id]) {
+    return (
+      <div>
+        <h2>{patients[id].name}</h2>
+        <h4>ssn: {patients[id].ssn}</h4>
+        <h4>occupation: {patients[id].occupation}</h4>
+        <h3>Entries</h3>
+  
+        {patients[id].entries?.map(entry => (
+          <div>
+            <p>{entry.date} {entry.description}</p>
+            <ul>
+              {entry.diagnosisCodes &&
+               <ul>
+               {entry.diagnosisCodes.map((code) => (
+                 <li key={code}>{code}</li>
+               ))}
+             </ul>}
+            </ul>
+          </div>
+        ))}
+      </div>
+    )
+  } else {
+    return null
+  }
 }
 
 export default PatientList;
--- week9/patientor/frontend/src/types.ts
+++ week9/patientor/frontend/src/types.ts
@@ -15,7 +15,56 @@ export interface Patient {
   name: string;
   occupation: string;
   gender: Gender;
-  entries?: Array<string>;
+  entries?: Entry[]
   ssn?: string;
   dateOfBirth?: string;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>
+export type NonSensitivePatients = Omit<Patient, 'ssn'>;
+export type NewPatient = Omit<Patient, 'id' | 'entries'>
+
+export interface Diagnose {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string,
+    endDate: string,
+  }
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  }
 }
