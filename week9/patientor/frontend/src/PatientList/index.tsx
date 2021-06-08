06c7f157a705f0907625d9b09ded90427274b7c6
henripalin <henri.j.palin@gmail.com>
2021-06-08 22:12:08 +0300

    9.21

--- week9/patientor/frontend/src/PatientList/index.tsx
+++ week9/patientor/frontend/src/PatientList/index.tsx
@@ -3,23 +3,37 @@ import { apiBaseUrl } from "../constants";
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useStateValue, setPatient } from "../state";
+import { Diagnosis } from "../types";
 
 const PatientList: React.FC = () => {
-  const { id } = useParams<{id: string}>();
-  const [{ patients }, dispatch] = useStateValue();
-  const url = `${apiBaseUrl}/patients/${id}`
+  const { id } = useParams<{id: string}>()
+  const [{ patients }, dispatch] = useStateValue()
+  const patientUrl = `${apiBaseUrl}/patients/${id}`
+  const diagnoseUrl = `${apiBaseUrl}/diagnoses/`
+  const [diagnosesCodes, setDiagnosesCodes] = React.useState<Diagnosis[]>();
+
 
   useEffect(() => {
     const getPatient = async () => {
-      const { data: fetchedPatient } = await axios.get(url);
+      const { data: fetchedPatient } = await axios.get(patientUrl)
       console.log(fetchedPatient)
-      dispatch(setPatient(fetchedPatient));
+      dispatch(setPatient(fetchedPatient))
+
+      const fetchedCodes = await axios.get<Diagnosis[]>(diagnoseUrl)
+      console.log(fetchedCodes)
+      setDiagnosesCodes(fetchedCodes.data);
     }
 
     if (patients[id]?.entries === undefined) {
       getPatient();
     }
-  }, []);
+  }, [])
+
+  const getDiagnosisDescription = (code: string): string => {
+    if (!diagnosesCodes) return '';
+    const diagnosis = diagnosesCodes.find(d => d.code === code);
+    return '' + diagnosis?.name
+  }
 
   if (patients[id]) {
     return (
@@ -36,7 +50,7 @@ const PatientList: React.FC = () => {
               {entry.diagnosisCodes &&
                <ul>
                {entry.diagnosisCodes.map((code) => (
-                 <li key={code}>{code}</li>
+                 <li key={code}>{code} {getDiagnosisDescription(code)}</li>
                ))}
              </ul>}
             </ul>
