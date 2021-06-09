58e567696a633858b7867d271cb0cbcd3049d2b1
henripalin <henri.j.palin@gmail.com>
2021-06-09 22:52:46 +0300

    9.24-9.25

--- week9/patientor/frontend/src/PatientList/index.tsx
+++ week9/patientor/frontend/src/PatientList/index.tsx
@@ -3,16 +3,18 @@ import { apiBaseUrl } from "../constants";
 import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useStateValue, setPatient } from "../state";
-import { Diagnosis, Entry } from "../types";
-import { Icon, SemanticCOLORS } from 'semantic-ui-react';
+import { Diagnosis, Entry, Patient } from "../types";
+import { Icon, Button } from 'semantic-ui-react';
+import { EntryFormValues } from '../AddEntryModal/AddEntryForm';
+import AddEntryModal from '../AddEntryModal';
 
 const PatientList: React.FC = () => {
   const { id } = useParams<{id: string}>()
   const [{ patients }, dispatch] = useStateValue()
   const patientUrl = `${apiBaseUrl}/patients/${id}`
   const diagnoseUrl = `${apiBaseUrl}/diagnoses/`
-  const [diagnosesCodes, setDiagnosesCodes] = React.useState<Diagnosis[]>();
-
+  const [modalOpen, setModalOpen] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>();
 
   useEffect(() => {
     const getPatient = async () => {
@@ -22,13 +24,30 @@ const PatientList: React.FC = () => {
 
       const fetchedCodes = await axios.get<Diagnosis[]>(diagnoseUrl)
       console.log(fetchedCodes)
-      setDiagnosesCodes(fetchedCodes.data);
     }
 
     if (patients[id]?.entries === undefined) {
       getPatient();
     }
   }, [])
+
+  const openModal = (): void => setModalOpen(true);
+
+  const closeModal = (): void => {
+    setModalOpen(false);
+    setError(undefined);
+  };
+
+  const submitNewEntry = async (values: EntryFormValues) => {
+    try {
+      const { data: patient } = await axios.post<Patient>(`${patientUrl}/entries`, values);
+      console.log(patient)
+      dispatch({ type: "UPDATE_PATIENT", payload: patient });
+      closeModal();
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  };
 
   if (patients[id]) {
     return (
@@ -38,10 +57,17 @@ const PatientList: React.FC = () => {
         <h4>occupation: {patients[id].occupation}</h4>
         <h3>Entries</h3>
         {patients[id].entries?.map(entry => (
-          <div>
+          <div key = {entry.id}>
             <EntryDetails key={entry.id} entry={entry}/>
           </div>
         ))}
+        <AddEntryModal
+          modalOpen={modalOpen}
+          onSubmit={submitNewEntry}
+          error={error}
+          onClose={closeModal}
+        />
+        <Button onClick={() => openModal()}>Add entry</Button>
       </div>
     )
   } else {
